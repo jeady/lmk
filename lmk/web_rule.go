fa4a1ce165b47782c57b640d1584a73d9ba8a736
James Eady <jmeady@gmail.com>
2013-06-03 14:05:49 -0700

    Implement WebRule.TestTriggered().

--- lmk/web_rule.go
+++ lmk/web_rule.go
@@ -1,6 +1,11 @@
 package lmk
 
-import "strconv"
+import (
+  "io/ioutil"
+  "net/http"
+  "strconv"
+  "strings"
+)
 
 type WebRule struct {
   name string
@@ -51,8 +56,47 @@ func (r *WebRule) set_options(opts map[string]string) {
   }
 }
 
+func (r *WebRule) contains(content, substr string) bool {
+  if !r.case_sensitive {
+    content = strings.ToLower(content)
+    substr = strings.ToLower(substr)
+  }
+  return strings.Contains(content, substr)
+}
+
+func (r *WebRule) test_sane(page_content string) bool {
+  return r.contains(page_content, r.sanity_check)
+}
+
+func (r *WebRule) test_triggered(page_content string) bool {
+  return r.contains(page_content, r.trigger_check)
+}
+
 func (r *WebRule) TestTriggered() (sane, triggered bool) {
-  sane = false
-  triggered = false
+  resp, err := http.Get(r.url)
+  if err != nil {
+    log.Info("WebRule '%s' not sane because of error GET'ing page: %s",
+      r.Name(),
+      err.Error())
+    sane = false
+    triggered = false
+    return
+  }
+  defer resp.Body.Close()
+
+  page_bytes, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    log.Info("WebRule '%s' not sane because of error reading body: ",
+      r.Name(),
+      err)
+    sane = false
+    triggered = false
+    return
+  }
+
+  page := string(page_bytes)
+
+  sane = r.test_sane(page)
+  triggered = r.test_triggered(page)
   return
 }
