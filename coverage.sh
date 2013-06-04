c13aa2a8ba7c3563fd62f64e34b3bd7a4148c5f1
James Eady <jmeady@gmail.com>
2013-06-04 16:21:07 -0700

    Fix coverage script to point at lmk/engine.

--- coverage.sh
+++ coverage.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-gocov test github.com/jeady/lmk/lmk > /tmp/lmk.cov.json
+gocov test github.com/jeady/lmk/engine > /tmp/lmk.cov.json
 gocov-html /tmp/lmk.cov.json > /tmp/lmk.cov.html
 open /tmp/lmk.cov.html
